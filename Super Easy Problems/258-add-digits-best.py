a11ed99aedc50a4f608e5560a81c4743262e7655
Silver Alcid <silveralcid@gmail.com>
2025-10-06 06:20:02 -0400

    258. Add Digits

---
+++ Super Easy Problems/258-add-digits-best.py
@@ -0,0 +1,7 @@
+# use digital root formula from number theory
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        return 1 + (num - 1) % 9
---
+++ Super Easy Problems/258-add-digits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num >= 10:
+            digits = []
+            for d in str(num):
+                digits.append(int(d))
+            num = sum(digits)
+        return num
